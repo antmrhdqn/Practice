f482a70a65a1ae2264db45244524479728b7e10c
antmrhdqn <antmrhdqn@gmail.com>
2023-12-18 18:07:48 +0900

    chap04

---
+++ switch-case/src/com/ohgiraffers/practice1/Application.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.practice1;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+        switchCase a = new switchCase();
+
+        a.switchVendingMachine();
+    }
+
+
+
+}
