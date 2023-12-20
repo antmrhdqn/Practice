1a372898e5cda9d7745fc6422f7ebfb16770f54a
antmrhdqn <antmrhdqn@gmail.com>
2023-12-20 19:07:40 +0900

    8 8

--- chap04-control-flow-statement-practice/src/com/ohgiraffers/practice/looping/Application9.java
+++ chap04-control-flow-statement-practice/src/com/ohgiraffers/practice/looping/Application9.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.practice.looping;
 
+import java.util.Scanner;
+
 public class Application9 {
 
     public static void main(String[] args) {
@@ -31,6 +33,12 @@ public class Application9 {
          * -- 출력 예시 --
          * 영문자가 아닌 문자가 포함되어 있습니다.
          * */
+        Scanner sc = new Scanner(System.in);
+        System.out.print("문자열 입력 : ");
+        String str = sc.nextLine();
+        System.out.print("문자 입력 : ");
+        char ch = sc.nextLine().charAt(0);
+
 
     }
 
