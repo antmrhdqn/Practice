88e441aa43c964a6f2d65d40ae107af8f3a8cef3
antmrhdqn <antmrhdqn@gmail.com>
2024-01-15 00:12:35 +0900

    12

---
+++ chap12-collection-practice-source/src/com/ohgiraffers/hw2/controller/BookManager.java
@@ -0,0 +1,35 @@
+package com.ohgiraffers.hw2.controller;
+
+import com.ohgiraffers.hw2.model.dto.BookDTO;
+
+import java.util.ArrayList;
+
+public class BookManager {
+    public void addBook(BookDTO bookDTO) {
+
+    }
+
+    public void deleteBook(int index) {
+
+    }
+
+    public int searchBook(String bTitle) {
+
+    }
+
+    public void printBook(int index) {
+
+    }
+
+    public void displayAll() {
+
+    }
+
+    public ArrayList<BookDTO> sortedBookList(int select) {
+
+    }
+
+    public void printBookList(ArrayList<BookDTO> br) {
+
+    }
+}
---
+++ chap12-collection-practice-source/src/com/ohgiraffers/hw2/run/Application.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.hw2.run;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+}
