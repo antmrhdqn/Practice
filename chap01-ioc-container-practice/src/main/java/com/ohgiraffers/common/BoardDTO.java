94f3644aaf9d88411fd3b5d123b628852f5bd6e5
antmrhdqn <antmrhdqn@gmail.com>
2024-02-14 10:24:44 +0900

    1-1

---
+++ chap01-ioc-container-practice/src/main/java/com/ohgiraffers/common/BoardDTO.java
@@ -0,0 +1,61 @@
+package com.ohgiraffers.common;
+
+public class BoardDTO {
+
+    private Long id; //아이디
+    private String title; //제목
+    private String content; //내용
+    private String writer; //작성자
+
+    public BoardDTO() {
+    }
+
+    public BoardDTO(Long id, String title, String content, String writer) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    @Override
+    public String toString() {
+        return "BoardDTO{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", writer='" + writer + '\'' +
+                '}';
+    }
+}
